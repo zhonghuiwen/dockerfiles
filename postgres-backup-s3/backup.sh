db8a76f68abad2cfe6063bc8dd5d716029442290
Zhong Huiwen <huiwen@aporigin.com>
2019-10-07 14:30:15 +0800

    Update backup.sh

--- postgres-backup-s3/backup.sh
+++ postgres-backup-s3/backup.sh
@@ -65,4 +65,4 @@ echo "Uploading dump to $S3_BUCKET"
 
 cat dump.sql.gz | aws $AWS_ARGS s3 cp - s3://$S3_BUCKET/$S3_PREFIX/${POSTGRES_DATABASE}_$(date +"%Y-%m-%dT%H:%M:%SZ").sql.gz || exit 2
 
-echo "SQL backup uploaded successfully"
+echo "Postgres backup uploaded successfully"
